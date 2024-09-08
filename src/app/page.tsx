7a95c04094598048c506374a882b67a0a3a54bf9
RYU <takahashi.ryunosuke.job@gmail.com>
2024-09-08 13:02:13 +0900

    feat: update home layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
 export default function Home() {
   return (
-    <article>
-      <h1>Simple Image</h1>
-    </article>
+    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-sans sm:p-20">
+      <header>
+        <h1>Simple Image</h1>
+      </header>
+      <main className="row-start-2 flex flex-col items-center gap-8 sm:items-start"></main>
+      <footer className="row-start-3 flex flex-wrap items-center justify-center gap-6"></footer>
+    </div>
   )
 }
