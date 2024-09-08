c5f617f20de049196703f81a707ffba204241282
RYU <takahashi.ryunosuke.job@gmail.com>
2024-09-08 13:12:21 +0900

    feat: upadate home layput

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 export default function Home() {
   return (
-    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-sans sm:p-20">
+    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 bg-background p-8 pb-20 font-sans sm:p-20">
       <header>
         <h1>Simple Image</h1>
       </header>
       <main className="row-start-2 flex flex-col items-center gap-8 sm:items-start"></main>
-      <footer className="row-start-3 flex flex-wrap items-center justify-center gap-6"></footer>
+      <footer className="row-start-3 flex flex-wrap items-center justify-center gap-6">
+        Next.js + FastAPI (OpenCV + OpenAI API)
+      </footer>
     </div>
   )
 }
